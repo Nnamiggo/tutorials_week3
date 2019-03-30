7d7be6c9903042fff5f3abd4adf0c89028742214
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-30 12:43:06 +0300

    practicing

---
+++ attributes_script.js
@@ -0,0 +1,9 @@
+
+// Assign image element
+const img = document.querySelector('img');
+
+console.log(img.hasAttribute('src'));
+img.getAttribute('src');
+img.removeAttribute('src');
+img.setAttribute('src', 'img2.jpg');
+img.src="img2.jpg"
--- script.js
+++ script.js
@@ -10,3 +10,12 @@ let button = document.getElementById('changeBackground');
 button.addEventListener('click', () => {
   document.body.style.backgroundColor = 'fuchsia';
 });
+
+// Assign image element
+const img = document.querySelector('img');
+
+console.log(img.hasAttribute('src'));
+img.getAttribute('src');
+img.removeAttribute('src');
+img.setAttribute('src', 'img2.jpg');
+img.src="img1.jpg"
